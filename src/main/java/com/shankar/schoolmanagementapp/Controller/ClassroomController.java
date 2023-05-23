3bf38dba0a5fea80b904e64c66a3521d1cf0b0df
shankaran314 <shankaran.er@outlook.com>
2023-05-23 11:07:08 +0530

    ManyToMany for Teachers, classrooms and students

--- src/main/java/com/shankar/schoolmanagementapp/Controller/ClassroomController.java
+++ src/main/java/com/shankar/schoolmanagementapp/Controller/ClassroomController.java
@@ -8,9 +8,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.shankar.schoolmanagementapp.dao.ClassroomRepository;
+import com.shankar.schoolmanagementapp.dao.StudentRepository;
+import com.shankar.schoolmanagementapp.dao.TeacherRepositroy;
 import com.shankar.schoolmanagementapp.entities.Classroom;
+import com.shankar.schoolmanagementapp.entities.Student;
+import com.shankar.schoolmanagementapp.entities.Teacher;
 
 @Controller
 @RequestMapping("/classrooms")
@@ -19,19 +24,36 @@ public class ClassroomController {
     @Autowired
     ClassroomRepository classroomRepo;
 
+    @Autowired
+    TeacherRepositroy teacherRepo;
+
+    @Autowired
+    StudentRepository studentRepo;
+
     @GetMapping
     public String displayClassroom(Model model){
         Classroom classroom = new Classroom();
         List<Classroom> classrooms = classroomRepo.findAll();
-        System.out.println(classrooms);
+        List<Student> students = studentRepo.findAll();
+        List<Teacher> teachers = teacherRepo.findAll();
         model.addAttribute("classroom", classroom);
         model.addAttribute("classrooms", classrooms);
+        model.addAttribute("teachers", teachers);
+        model.addAttribute("students", students);
         return "classroom/classroom";
     }
 
     @PostMapping("/save")
-    public String createClassroom(Model model, Classroom classroom){
+    public String createClassroom(Model model, Classroom classroom, @RequestParam(value="students") List<Integer>studentIds){
         classroomRepo.save(classroom);
+
+        Iterable<Student> classStudents = studentRepo.findAllById(studentIds);
+
+        for(Student stu : classStudents){
+            stu.setClassroom(classroom);
+            studentRepo.save(stu);
+        }
+
         return "redirect:/classrooms";
     }
     
