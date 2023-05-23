d320f3f81214cc809a90fec69d56810f44a52705
shankaran314 <shankaran.er@outlook.com>
2023-05-23 10:14:53 +0530

    Student save

--- src/main/java/com/shankar/schoolmanagementapp/Controller/HomeController.java
+++ src/main/java/com/shankar/schoolmanagementapp/Controller/HomeController.java
@@ -1,14 +1,25 @@
 package com.shankar.schoolmanagementapp.Controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.shankar.schoolmanagementapp.dao.StudentRepository;
+import com.shankar.schoolmanagementapp.entities.Student;
 
 @Controller
 public class HomeController {
     
+    @Autowired
+    StudentRepository studentRepo;
 
     @GetMapping("/")
-    public String displayHomePage(){
+    public String displayHomePage(Model model){
+        List<Student> students = studentRepo.findAll();
+        model.addAttribute("students", students);
         return "index.html";
     }
 }
--- src/main/java/com/shankar/schoolmanagementapp/Controller/StudentController.java
+++ src/main/java/com/shankar/schoolmanagementapp/Controller/StudentController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.shankar.schoolmanagementapp.dao.StudentRepository;
@@ -18,16 +19,21 @@ public class StudentController {
     @Autowired
     StudentRepository studentRepo;
 
-
     @GetMapping
-    public String displayStudent(Model model){
+    public String displayStudent(Model model) {
 
         Student student = new Student();
         List<Student> students = studentRepo.findAll();
         model.addAttribute("students", students);
         model.addAttribute("student", student);
-
         return "student/student";
     }
-    
+
+    @PostMapping("/save")
+    public String createStudent(Model model, Student student) {
+        studentRepo.save(student);
+        return "redirect:/students";
+
+    }
+
 }
