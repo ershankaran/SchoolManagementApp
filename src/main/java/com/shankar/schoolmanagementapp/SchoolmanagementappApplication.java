70c346835f50a1beb5f5b57e06daa7b77116611c
shankaran314 <shankaran.er@outlook.com>
2023-06-04 07:01:36 +0530

    Component Scanning.repository addition

--- src/main/java/com/shankar/schoolmanagementapp/SchoolmanagementappApplication.java
+++ src/main/java/com/shankar/schoolmanagementapp/SchoolmanagementappApplication.java
@@ -3,7 +3,7 @@ package com.shankar.schoolmanagementapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.shankar.schoolmanagementapp","com.shankar.utils"})
 public class SchoolmanagementappApplication {
 
 	public static void main(String[] args) {
