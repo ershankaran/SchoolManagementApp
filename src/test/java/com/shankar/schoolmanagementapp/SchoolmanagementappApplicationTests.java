bb4c415f68a67242fe31da5b6cd05087c76b6600
shankaran314 <shankaran.er@outlook.com>
2023-06-05 19:02:51 +0530

    external configuration

---
+++ src/test/java/com/shankar/schoolmanagementapp/SchoolmanagementappApplicationTests.java
@@ -0,0 +1,15 @@
+package com.shankar.schoolmanagementapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+@SpringBootTest
+// @TestPropertySource("/src/main/resources/application.properties")
+class SchoolmanagementappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
