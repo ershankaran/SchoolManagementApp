a35cb3966bbda680cd27f8da71f105cf7051d8e8
shankaran314 <shankaran.er@outlook.com>
2023-06-06 19:55:17 +0530

    using springboottest annotation

---
+++ src/test/java/com/shankar/schoolmanagementapp/dao/ClassroomRepositoryIntegrationTest.java
@@ -0,0 +1,37 @@
+package com.shankar.schoolmanagementapp.dao;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.shankar.schoolmanagementapp.dao.ClassroomRepository;
+import com.shankar.schoolmanagementapp.entities.Classroom;
+
+@SpringBootTest
+// @ExtendWith(SpringExtension.class)
+@RunWith(SpringRunner.class)
+
+public class ClassroomRepositoryIntegrationTest {
+
+    @Autowired
+    ClassroomRepository classroomRepo;
+
+    @Test
+    public void ifNewClassroomSaved_thenSuccess(){
+        Classroom newClassroom = new Classroom("Test Classroom","testclsroom@tst.org");
+        classroomRepo.save(newClassroom);
+        List<Classroom> testdata = classroomRepo.findAll();
+        testdata.forEach((n) -> System.out.println(n.getClassroomName()));
+        assertEquals(1, classroomRepo.findAll().size());
+    }
+    
+}
